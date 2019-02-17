4bb74e0eed7553d4a82354b5b979d26b75202b50
Matt Kross <krosswiredtech@gmail.com>
2019-02-16 20:44:21 -0500

    Added footer info

--- footer.php
+++ footer.php
@@ -1,3 +1,15 @@
 
-</body>
+<footer class="site-footer">
+
+    <!-- dynamically adds date and name -->
+    <p>
+        <?php bloginfo('name'); ?>
+            - &copy;
+        <?php echo date('Y'); ?>
+    </p>
+</footer>
+
+<?php wp_footer(); ?>
+
+    </body>
 </html>
